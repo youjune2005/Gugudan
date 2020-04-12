cb0c2808f74295c401fdf32d141309f6a3400b50
youju <youju@DESKTOP-FF3HUVI>
2020-04-12 19:17:23 +0900

    4단과 입력한 단 출력 추가

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
+		
+		/*
 		//2단
+		
 		System.out.println("2단");
 		System.out.println(2 * 1);
 		System.out.println(2 * 2);
@@ -24,7 +28,43 @@ public class Gugudan {
 		System.out.println(3 * 7);	
 		System.out.println(3 * 8);
 		System.out.println(3 * 9);
-		
+		*/
+		/*
 		//4단
+		System.out.println("4단");
+		int result = 4*1;
+		System.out.println(result);
+		result = 4*2;
+		System.out.println(result);
+		result = 4*3;
+		System.out.println(result);
+		result = 4*4;
+		System.out.println(result);
+		result = 4*5;
+		System.out.println(result);
+		result = 4*6;
+		System.out.println(result);
+		result = 4*7;
+		System.out.println(result);
+		result = 4*8;
+		System.out.println(result);
+		result = 4*9;
+		System.out.println(result);
+		*/
+		
+		
+		//입력한 단 출력하기
+		System.out.println("구구단중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
 	}
 }
