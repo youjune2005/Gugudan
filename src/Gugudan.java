e9312eec2252d9df679f9e1f107bf324bfb403f1
youju <youju@DESKTOP-FF3HUVI>
2020-04-19 23:46:51 +0900

    조건문을 추가한 8단, 9단

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -77,10 +77,32 @@ public class Gugudan {
 				}
 		*/
 		
+		/*
 		//7단
 		for(int i=1;i<1000;i++) {
 			System.out.println(7*i);
 			}
+		*/
+		
+		//8단,9단
+		System.out.println("구구단 중 출력할 단은? :");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : " + number);
+		
+		
+		if(number<2) {
+			System.out.println("값을 잘못 입력했습니다.");
+		}
+		    else if(number>9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		    }
+			else {
+				for(int i=1;i<10;i++) {
+					System.out.println(number*i);
+				}
+			}
+		
 	}
 }
 
