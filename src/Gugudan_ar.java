49f82dd29d075008c693e3ce85720ebb87c8abcb
youju <youju@DESKTOP-FF3HUVI>
2020-04-20 00:09:15 +0900

    배열을 이용해서 1단부터 4단까지 구연하고 2중for문을 이용하여 1단부터 9단까지 구현

--- src/Gugudan_ar.java
+++ src/Gugudan_ar.java
@@ -1,4 +1,56 @@
 
 public class Gugudan_ar {
-
+	public static void main(String[] args) {
+		
+//		//2단
+//		int[] result = new int[9];
+//		
+//		for(int i=0; i < result.length; i++ ) {
+//			result[i] = 2 * (i+1);
+//		}
+//		
+//		for(int i=0; i<result.length;i++) {
+//			System.out.println(result[i]);
+//		}
+//		
+//		//3단
+//		result = new int[9];
+//		
+//		for(int i=0; i < result.length; i++ ) {
+//			result[i] = 3 * (i+1);
+//		}
+//		
+//		for(int i=0; i<result.length;i++) {
+//			System.out.println(result[i]);
+//		}
+//		
+//		//4단
+//		result = new int[9];
+//		
+//		for(int i=0; i < result.length; i++ ) {
+//			result[i] = 4 * (i+1);
+//		}
+//		
+//		for(int i=0; i<result.length;i++) {
+//			System.out.println(result[i]);
+//		}
+//		
+		
+		//2중for문으로 구구단 구현 1단부터 9단까지
+		int[][] result = new int[9][9];
+		
+		for(int i=0;i<result.length;i++) {
+			for(int j=0;j<result.length;j++) {
+				result[i][j]= (i + 1) * (j + 1);
+			}
+		}
+		
+		for(int i=0;i<result.length;i++) {
+			for(int j=0;j<result.length;j++) {
+				System.out.println(result[i][j]);
+			}
+		}
+		
+		
+	}
 }
