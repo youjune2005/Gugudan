0cd3b5705c963450258b1b8321c381cd706558d9
youju <youju@DESKTOP-FF3HUVI>
2020-04-20 01:35:28 +0900

    추가 미션 수행하기

---
+++ src/Gugudan_str.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Gugudan_str {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		
+	}
+	
+}
